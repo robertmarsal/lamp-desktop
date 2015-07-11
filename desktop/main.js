7122647203da0f4aaf8ad4b407939ff53b040e4b
Robert Boloc <robertboloc@gmail.com>
2015-07-11 18:53:38 +0100

    first commit

---
+++ desktop/main.js
@@ -0,0 +1,18 @@
+var app           = require('app');
+var BrowserWindow = require('browser-window');
+
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+    if (process.platform != 'darwin') {
+      app.quit();
+    }
+});
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({width: 1178, height: 880});
+    mainWindow.loadUrl('file://' + __dirname + '/index.html');
+    mainWindow.on('closed', function() {
+      mainWindow = null;
+    });
+});
