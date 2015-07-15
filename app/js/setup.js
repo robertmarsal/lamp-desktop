65ac9d8cf3d38b9f0eda1fab1c5f5fa99050dacd
Robert Boloc <robertboloc@gmail.com>
2015-07-15 20:57:28 +0100

    Make sure main libray folder is created before using

--- app/js/setup.js
+++ app/js/setup.js
@@ -1,20 +1,23 @@
 var osenv = require('osenv');
+var path  = require('path');
 var fs    = require('fs');
 var books = require('./js/library');
 
 global.library = osenv.home() + '/.lamp';
 
 var libraryFolders = [
+    // Main library folder
+    global.library,
     // All the user books are stored here
-    'books',
+    path.join(global.library, 'books'),
     // Cache of the book covers
-    'cache'
+    path.join(global.library, 'cache')
 ];
 
 // Make sure all the library folders exist
 libraryFolders.forEach(function(folder){
-    if (!fs.existsSync(global.library + '/' + folder)) {
-        fs.mkdirSync(global.library + '/' + folder);
+    if (!fs.existsSync(folder)) {
+        fs.mkdirSync(folder);
     }
 });
 
